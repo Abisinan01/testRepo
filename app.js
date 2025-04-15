969e6f0b5e70b0730b6a43f289281837626e41d0
Abisinan <abisinanmadathil0@gmail.com>
2025-04-15 14:40:34 +0530

    chore:calculate values

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-var arithmeticTriplets = function(nums, diff) {
-    let res = [], j=0
-    
-    for(let i=0; i<nums.length; i++){
-        for(j=0; j<nums.length; j++){
-            for(let k=j+1;k<nums.length; k++){
-        
-                if(diff == nums[k] - nums[j]){
-                    console.log(1)
-                }
-            }
-        }
+function func(){
+    let x = 10
+
+    function myfunc(){
+        console.log(x)
     }
-    return res
-};
-console.log(arithmeticTriplets([0,1,4,6,7,10],3))
+    myfunc()
+}
+
+func()
